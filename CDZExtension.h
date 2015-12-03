522ef468c1c6805eb611cbb328a7e94f62d89bf7
baight <cz-cczz@163.com>
2015-12-03 11:30:04 +0800

    code

--- CDZExtension.h
+++ CDZExtension.h
@@ -6,7 +6,7 @@
 //  Copyright (c) 2014年 baight. All rights reserved.
 //
 
-#import <Foundation/Foundation.h>
+#import <UIKit/UIKit.h>
 #import <AssetsLibrary/AssetsLibrary.h>
 
 #define RGB(r,g,b)      [UIColor colorWithRed:(r)/255.0 green:(g)/255.0 blue:(b)/255.0 alpha:1]
@@ -28,6 +28,7 @@
 
 
 #define StringNotNil(str) (str ? str : @"")
+#define ArrayNotNil(arr) (arr ? arr : @[])
 
 #pragma mark - UIView
 @interface UIView (CDZViewExtension)
